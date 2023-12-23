b262f2035622760cc977381bdb90eefd35be7fee
Jared Asuncion <guissmo@gmail.com>
2023-12-23 23:21:48 +0100

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,52 @@ import "./globals.css";
 
 const font = Peralta({ weight: "400", subsets: ["latin"] });
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
+
 export const metadata: Metadata = {
-  title: "Corninator 3000",
-  description: "Jokes that will make you cool.",
+  title: "Corninator | Funny Jokes",
+  description:
+    "Ever needed funny jokes to make you look cool at parties? Search no more!",
+  creator: "Jared Asuncion",
+  applicationName: "Corninator",
+  keywords: ["Corninator", "jokes", "funny jokes"],
+  authors: [{ name: "Jared Guissmo Asuncion", url: "https://guissmo.com" }],
+  icons: {
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
+    apple: "/favicon.png",
+    other: {
+      rel: "apple-touch-icon-precomposed",
+      url: "/favicon.png",
+    },
+  },
+  openGraph: {
+    title: "Corninator | Funny Jokes",
+    description:
+      "Ever needed funny jokes to make you look cool at parties? Search no more!",
+    url: "https://jokes.guissmo.com",
+    siteName: "Corninator",
+    images: [
+      {
+        url: "/corninator-og-image.png",
+        width: 745,
+        height: 745,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Corninator | Funny Jokes",
+    description:
+      "Ever needed funny jokes to make you look cool at parties? Search no more!",
+    images: ["/corninator-og-image.png"],
+  },
 };
 
 export default function RootLayout({
